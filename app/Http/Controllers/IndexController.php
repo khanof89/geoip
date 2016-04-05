6fbe38083018033f0dbce7568a2e9fccb0ced96e
Shahrukh Khan <khan_shahrukh89@yahoo.com>
2016-04-05 15:11:39 +0530

    First commit

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class IndexController extends Controller
+{
+    public function index(Request $request)
+    {
+        $ip = $request->ip;
+        $type = $request->type;
+        $location = \GeoIP::getLocation($ip);
+        return $location[$type];
+    }
+}
